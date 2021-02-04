a4ae7519ff4d94f099f1929c2d202550d0b9861c
islambou9427 <islem.bouagada@gmail.com>
2021-02-04 19:53:20 +0100

    Initial Commit

---
+++ Food Delivery Ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food Delivery Ui
+//
+//  Created by mac on 2/4/21.
+//  Copyright © 2021 mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
