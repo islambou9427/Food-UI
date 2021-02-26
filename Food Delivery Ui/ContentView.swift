43bd78a62e4276aaadcb99d8490175dc298b4826
islambou9427 <islem.bouagada@gmail.com>
2021-02-26 22:07:39 +0100

    finish bottom section "SmallRecipeCardView"

--- Food Delivery Ui/ContentView.swift
+++ Food Delivery Ui/ContentView.swift
@@ -12,28 +12,47 @@ struct ContentView: View {
     
     @State private var search : String=""
     var body: some View {
-        VStack (alignment: .leading , spacing: 16) {
-            
-            HomeNavBarView()
-            Text("Bonjour, Islam")
-                .font(.system(size: 20.0))
-                .fontWeight(.medium)
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            Text("What ould you like to eat today ? ")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-            
-            SearchAndFilterView(search: $search)
-            
-            TabTitleView(title: "Today's Fresh Recipes")
-            
-            
-            
-            Spacer()
-            
-            
+        ScrollView (.vertical , showsIndicators:false) {
+            VStack (alignment: .leading , spacing: 16) {
+                
+                HomeNavBarView()
+                Text("Bonjour, Islam")
+                    .font(.system(size: 20.0))
+                    .fontWeight(.medium)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                Text("What ould you like to eat today ? ")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                
+                SearchAndFilterView(search: $search)
+                
+                TabTitleView(title: "Today's Fresh Recipes")
+                
+                ScrollView (.horizontal,showsIndicators:false) {
+                    HStack (spacing:16) {
+                        BigRecipeCardView(image: #imageLiteral(resourceName: "fresh_recipe_1"))
+                        BigRecipeCardView(image: #imageLiteral(resourceName: "fresh_recipe_2"))
+                        BigRecipeCardView(image: #imageLiteral(resourceName: "reco_3"))
+                        BigRecipeCardView(image: #imageLiteral(resourceName: "reco_2"))
+                    }
+                }
+                
+                TabTitleView(title: "Recommended")
+                
+                VStack {
+                    SmallRecipeCardView(image: #imageLiteral(resourceName: "reco_2"), title: "French Toast with Berries")
+                    SmallRecipeCardView(image: #imageLiteral(resourceName: "reco_3"), title: "English Toast with Berries")
+                    SmallRecipeCardView(image: #imageLiteral(resourceName: "reco_1"), title: "English Toast with Berries")
+                }
+                
+                
+     
+                Spacer()
+                
+                
+            }
+            .padding()
         }
-        .padding()
         
     }
 }
@@ -75,12 +94,12 @@ struct SearchAndFilterView: View {
                 
             }
             .padding()
-            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+            .background(Color("LightGrayColor"))
             .cornerRadius(8)
             
             Image(uiImage: #imageLiteral(resourceName: "filter"))
                 .padding()
-                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                .background(Color("LightGrayColor"))
                 
                 .cornerRadius(8)
         }
@@ -105,5 +124,123 @@ struct TabTitleView: View {
             
             
         }
+    }
+}
+
+struct BigRecipeCardView: View {
+    let image: UIImage
+    var body: some View {
+        ZStack {
+            VStack (alignment: .leading, spacing: 8) {
+                
+                Image(uiImage: #imageLiteral(resourceName: "like"))
+                    .padding(.bottom,60)
+                
+                Text("BreakFast")
+                    .font(.caption)
+                    .foregroundColor(Color(#colorLiteral(red: 0.0634567216, green: 0.1739850938, blue: 0.2506789267, alpha: 0.6780017671)))
+                Text("French Toast with Berries")
+                    .fontWeight(.medium)
+                
+                HStack (spacing: 2){
+                    ForEach(0..<5){ item in
+                        Image(uiImage: #imageLiteral(resourceName: "star"))
+                            .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(Color("PrimaryColor"))
+                        
+                    }
+                }
+                
+                Text("120 Calories")
+                    .font(.caption)
+                    .foregroundColor(Color("PrimaryColor"))
+                
+                
+                HStack{
+                    
+                    Image(uiImage: #imageLiteral(resourceName: "time"))
+                    Text("10 min")
+                        .font(.caption)
+                        .foregroundColor(Color("GrayColor"))
+                    Spacer()
+                    Image(uiImage: #imageLiteral(resourceName: "serving"))
+                    Text("1 Serving")
+                        .font(.caption)
+                        .foregroundColor(Color("GrayColor"))
+                    
+                }
+                
+                
+                
+                
+                
+            }
+            .frame(width: 147)
+            .padding()
+            .background(Color("LightGrayColor"))
+            .cornerRadius(20)
+            
+            Image(uiImage: image)
+                .offset(x:45,y:-60)
+        }.padding(.trailing,25)
+    }
+}
+
+struct SmallRecipeCardView: View {
+    let image: UIImage
+    let title:String
+    var body: some View {
+        HStack{
+            
+            Image(uiImage: image)
+                .aspectRatio(1,contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            
+            VStack (alignment: .leading, spacing:4){
+                
+                
+                
+                Text("BreakFast")
+                    .font(.caption)
+                    .foregroundColor(Color(#colorLiteral(red: 0.0634567216, green: 0.1739850938, blue: 0.2506789267, alpha: 0.6780017671)))
+                Text(title)
+                    .fontWeight(.medium)
+                
+                HStack (spacing: 2){
+                    ForEach(0..<5){ item in
+                        Image(uiImage: #imageLiteral(resourceName: "star"))
+                            .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(Color("PrimaryColor"))
+                        
+                    }
+                    Text("120 Calories")
+                        .font(.caption)
+                        .foregroundColor(Color("PrimaryColor"))
+                        .padding(.leading)
+                }
+                
+                
+                
+                
+                HStack{
+                    
+                    Image(uiImage: #imageLiteral(resourceName: "time"))
+                    Text("10 min")
+                        .font(.caption)
+                        .foregroundColor(Color("GrayColor"))
+                    
+                    Image(uiImage: #imageLiteral(resourceName: "serving"))
+                    Text("1 Serving")
+                        .font(.caption)
+                        .foregroundColor(Color("GrayColor"))
+                    Spacer()
+                    
+                }
+            }
+            
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(Color("LightGrayColor"))
+        .cornerRadius(18)
     }
 }
